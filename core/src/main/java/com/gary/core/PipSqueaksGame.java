fb236a17758f73344115754caecdeaafed04c787
Gary O'Donoghue <Gary@Garys-MacBook-Air.local>
2014-12-10 21:20:18 +0000

    add initial player select menu

--- core/src/main/java/com/gary/core/PipSqueaksGame.java
+++ core/src/main/java/com/gary/core/PipSqueaksGame.java
@@ -1,9 +1,10 @@
 package com.gary.core;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.Game;
 import com.gary.core.screens.GameScreen;
+import com.gary.core.screens.MenuScreen;
 
 public class PipSqueaksGame extends Game {
 	public static float delta;
@@ -11,7 +12,8 @@ public class PipSqueaksGame extends Game {
 	float elapsed;
 
 	private GameScreen gameScreen;
-
+	private MenuScreen menuScreen;
+	
 	public int screenHeight = 1080;
 
 	public int screenWidth = 1920;
@@ -21,11 +23,13 @@ public class PipSqueaksGame extends Game {
 	public void create() 
 	{
 		createScreens();
-		setScreen(gameScreen);
+		//setScreen(gameScreen);
+		setScreen(menuScreen);
 	}
 	
 	private void createScreens() 
 	{
-		this.gameScreen = new GameScreen(this);
+		//this.gameScreen = new GameScreen(this);
+		this.menuScreen = new MenuScreen(this);
 	}
 }
--- core/src/main/java/com/gary/core/screens/MenuScreen.java
+++ core/src/main/java/com/gary/core/screens/MenuScreen.java
@@ -1,80 +1,125 @@
 package com.gary.core.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.gary.core.PipSqueaksGame;
-
-public class MenuScreen implements Screen{
-
-	private static final int PIXELS_PER_METER = 10;
-	private PipSqueaksGame game;
+ 
+public class MenuScreen implements Screen
+{
+    private Stage stage = new Stage();
+    private SpriteBatch spriteBatch;
+    private PipSqueaksGame game;
 	private int screenWidth;
 	private int screenHeight;
 	private final int worldHeight;
+	private static final int PIXELS_PER_METER = 10;
 	private final int worldWidth;
 	private OrthographicCamera camera;
-	private SpriteBatch spriteBatch;
-	private Box2DDebugRenderer debugRenderer;
-	public static Vector2 center = new Vector2();
+	//private Texture logoTexture;
 
-	public MenuScreen(PipSqueaksGame game) {
-		this.game = game;
-
+	public MenuScreen(PipSqueaksGame pipSqueaksGame) {
+		this.spriteBatch = new SpriteBatch();
+		//this.logoTexture = new Texture(Gdx.files.internal("logo.png"));
+		this.game = pipSqueaksGame;
+		
 		this.screenWidth = this.game.screenWidth;
 		this.screenHeight = this.game.screenHeight;
 
 		this.worldHeight = screenHeight / PIXELS_PER_METER;
 		this.worldWidth = this.screenWidth / PIXELS_PER_METER;
 
-		center = new Vector2(worldWidth / 2, worldHeight / 2);
-
 		this.camera = new OrthographicCamera();
 		this.camera.setToOrtho(false, this.screenWidth, screenHeight);
+	}
 
-		this.spriteBatch = new SpriteBatch();
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0f, 0f, 0f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		this.spriteBatch.begin();
+		camera.update();
+		spriteBatch.setProjectionMatrix(camera.combined);
 
-		this.debugRenderer = new Box2DDebugRenderer();
+		//spriteBatch.draw(logoTexture, 0, 0);
+		
+		this.spriteBatch.end();
+		
+		stage.act(delta);
+	    stage.draw();
+	    
 	}
 	
 	@Override
-	public void render(float delta) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
+		Skin skin = new Skin( Gdx.files.internal( "defaultskin.json" ));
 		
+        Label welcomeLabel = new Label( "Press the A button to advance!", skin );
+        
+        welcomeLabel.setFontScale(2);
+        welcomeLabel.setX(width/3);
+        welcomeLabel.setY(( 80 ));
+        stage.addActor( welcomeLabel );
+        
+        // add the player identifiers in each corner, which will blink 
+        Label P1 = new Label( "Player 1", skin );
+        
+        P1.setFontScale(2);
+        P1.setX(10);
+        P1.setY(( height-60));
+        stage.addActor( P1 );
+        
+        Label P2 = new Label( "Player 2", skin );
+        
+        P2.setFontScale(2);
+        P2.setX(width-150);
+        P2.setY(( height-60));
+        stage.addActor( P2 );
+        
+        Label P3 = new Label( "Player 3", skin );
+        
+        P3.setFontScale(2);
+        P3.setX(10);
+        P3.setY((30));
+        stage.addActor( P3 );
+        
+        Label P4 = new Label( "Player 4", skin );
+        
+        P4.setFontScale(2);
+        P4.setX(width-150);
+        P4.setY((30));
+        stage.addActor( P4 );
 	}
-
 	@Override
 	public void show() {
-		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
 		
 	}
-
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
