d8340bca0f53f28fc5501afe8a43a0263ee88d82
Gary O'Donoghue <Gary@Garys-MacBook-Air.local>
2014-12-14 15:36:50 +0000

    Add logo. Fix dependency for controllers.

--- core/src/main/java/com/gary/core/screens/MenuScreen.java
+++ core/src/main/java/com/gary/core/screens/MenuScreen.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.controllers.Controller;
 import com.badlogic.gdx.controllers.Controllers;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -26,16 +27,17 @@ public class MenuScreen implements Screen
 	private int screenHeight;
 	private final int worldHeight;
 	private static final int PIXELS_PER_METER = 10;
+	private static final String PRESS_START = " Press Start";
 	private final int worldWidth;
 	private OrthographicCamera camera;
-	//private Texture logoTexture;
+	private Texture logoTexture;
 	private float blinkDelta = 0f;
 	private static int numberOfPlayers = 0;
 	private List<Player> listPlayers;
-	 
+	
 	public MenuScreen(PipSqueaksGame pipSqueaksGame) {
 		this.spriteBatch = new SpriteBatch();
-		//this.logoTexture = new Texture(Gdx.files.internal("logo.png"));
+		this.logoTexture = new Texture(Gdx.files.internal("title.png"));
 		this.game = pipSqueaksGame;
 		
 		this.screenWidth = this.game.screenWidth;
@@ -60,7 +62,7 @@ public class MenuScreen implements Screen
 		camera.update();
 		spriteBatch.setProjectionMatrix(camera.combined);
 
-		//spriteBatch.draw(logoTexture, 0, 0);
+		spriteBatch.draw(logoTexture, this.screenWidth/4, this.screenHeight/10, 1000, 1000);
 		
 		this.spriteBatch.end();
 		
@@ -69,21 +71,28 @@ public class MenuScreen implements Screen
 	    
 	    blinkText();
 	    
-	    assignControls();
+	    pollControllers();
 	    
 	}
 
-	private void assignControls() {
-		Gdx.app.log("Number of connected controllers: ", ""+Controllers.getControllers().size);
+	private void pollControllers() {
 		for (Controller controller : Controllers.getControllers()) {
 		    Gdx.app.log("controller connected with name: ", controller.getName());
-		   
+		    
 		    //check if player pressed the start button, create and assign a new player
 		    if(controller.getButton(Xbox360Pad.BUTTON_START)){
+		    	Gdx.app.log("Start button pressed","");
+		    	Gdx.app.log("Number of connected controllers: ", ""+Controllers.getControllers().size);
+		    	
 		    	Player player = new Player(controller);
 		    	if(listPlayers.contains(player)) break;
 		    	numberOfPlayers += 1;
 		    	listPlayers.add(player);
+		    }
+		    
+		    if(controller.getButton(Xbox360Pad.BUTTON_A)){
+		    	Gdx.app.log("A button pressed","");
+		    	Gdx.app.log("Start button pressed","");
 		    }
 		}
 	}
@@ -117,31 +126,31 @@ public class MenuScreen implements Screen
         stage.addActor( welcomeLabel );
         
         // add the player identifiers in each corner, which will blink 
-        Label P1 = new Label( "Player 1", skin );
+        Label P1 = new Label( "P1" + PRESS_START, skin );
         
         P1.setFontScale(2);
         P1.setX(10);
         P1.setY(( height-60));
         stage.addActor( P1 );
         
-        Label P2 = new Label( "Player 2", skin );
+        Label P2 = new Label( "P2" + PRESS_START, skin );
         
         P2.setFontScale(2);
-        P2.setX(width-150);
+        P2.setX(width-250);
         P2.setY(( height-60));
         stage.addActor( P2 );
         
-        Label P3 = new Label( "Player 3", skin );
+        Label P3 = new Label( "P3" + PRESS_START, skin );
         
         P3.setFontScale(2);
         P3.setX(10);
         P3.setY((30));
         stage.addActor( P3 );
         
-        Label P4 = new Label( "Player 4", skin );
+        Label P4 = new Label( "P4" + PRESS_START, skin );
         
         P4.setFontScale(2);
-        P4.setX(width-150);
+        P4.setX(width-250);
         P4.setY((30));
         stage.addActor( P4 );
 	}
